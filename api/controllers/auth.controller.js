c41fb4134f75435b0b45db732c98f66fbebeea67
Nitish kumar <nitish01011501kumar@gmail.com>
2025-01-05 13:32:40 +0530

    Created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 14);
@@ -20,4 +23,20 @@ export const signup = async (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found "));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const {password:pass,...rest} =validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res.cookie("access_token", token, { httpOnly: true })
+      .status(200).json({ rest });
+  } catch (err) {
+    next(err);
+  }
 };
