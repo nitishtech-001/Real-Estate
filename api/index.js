cce6cb41e95fe2c276cef4a16f15cad7a8bac084
Nitish kumar <nitish01011501kumar@gmail.com>
2025-02-23 00:29:05 +0530

    Add pre installation for deploy

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import listiningRoute from "./routes/listening.route.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
 //inisializing dotenv
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose
     console.log("Error connecting to MongoDb Database", err);
   });
 
+const __dirname = path.resolve()
 const app = express();
 
 app.listen(3000, () => {
@@ -27,14 +29,19 @@ app.listen(3000, () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRoute);
-app.use("/api/auth", authRoute);  
-app.use("/api/listening",listiningRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/listening", listiningRoute);
+
+app.use(express.static(path.join(__dirname,'/user/dist')));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"user","dist","index.html"));
+})
 //creating an middle-ware to handle errors
 app.use((err, req, res, next) => {
-  const statusCode =err.statusCode || 500;
-  const message=err.message || "Internal Server Error";
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
   });
