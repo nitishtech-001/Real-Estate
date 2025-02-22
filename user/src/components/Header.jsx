34063b7f9b4925a000f0a0d8d2b071256ea2ed8a
Nitish kumar <nitish01011501kumar@gmail.com>
2025-02-22 10:06:25 +0530

    update header search form

--- user/src/components/Header.jsx
+++ user/src/components/Header.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { ImSearch } from "react-icons/im";
 import {useSelector} from 'react-redux';
 export default function Header() {
+  const navigate = useNavigate();
   const {currentUser} = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm",searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate("/search?"+searchQuery);
+  }
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTerm = urlParams.get("searchTerm");
+    setSearchTerm(searchTerm || "");
+  },[window.location.search])
   return (
     <header className="block bg-slate-300 sm:bg-slate-200 ">
       <div className=" flex  justify-around items-center p-3 gap-4 sm:max-w-7xl sm:justify-between sm:gap-18 md:max-w-9xl">
@@ -11,13 +25,17 @@ export default function Header() {
           <span className="text-slate-500">Nitish</span>
           <span className="text-slate-700 md:mr-10">Estate</span>
         </h1>
-        <form className="flex items-center justify-between  min-w-8 max-w-21 bg-slate-100 rounded-md p-1 sm:p-3 sm:rounded-lg sm:max-w-1xl sm:flex-grow md:max-w-2xl " >
+        <form onSubmit={handleSubmit} className="flex items-center justify-between  min-w-8 max-w-21 bg-slate-100 rounded-md p-1 sm:p-3 sm:rounded-lg sm:max-w-1xl sm:flex-grow md:max-w-2xl " >
           <input
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
             type="text"
             placeholder="Search ..."
             className="bg-transparent   min-w-4 outline-none capitalize "
           />
+          <button>
           <ImSearch className="text-slate-500 size-4 sm:size-5" />
+          </button>
         </form>
         <ul className="flex items-center xs:gap-5 sm:gap-4  md:gap-6 lg:gap-8 xl:gap-10">
           <Link to="/">
